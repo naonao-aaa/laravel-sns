ae2fd506b1092222589d01da292ce714a84fc0e1
naonao-aaa <naonao1814naonao@gmail.com>
2023-04-15 16:47:50 +0900

    コントローラとモデルの編集（記事投稿機能）

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -17,5 +18,14 @@ class ArticleController extends Controller
     public function create()
     {
         return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;   //ログイン済みのユーザーが送信したリクエストであれば、Laravelではリクエストのuserメソッドを使うことでUserクラスのインスタンスにアクセスできる
+        $article->save();
+
+        return redirect()->route('articles.index');
     }
 }
